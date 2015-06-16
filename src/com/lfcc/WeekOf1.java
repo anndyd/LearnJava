416715fc33f2d21d0c25968ab090e6f630d5b85d
i063098 <anndy.dong@sap.com>
2015-06-17 07:25:38 +0800

    add comment

--- src/com/lfcc/WeekOf1.java
+++ src/com/lfcc/WeekOf1.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * ∆÷»Ì≈‡—µ Java øŒ≥Ã¡∑œ∞
+ * @author I063098
+ *
+ */
 public class WeekOf1 {
 	public static void main(String[] args) {
 		try {
